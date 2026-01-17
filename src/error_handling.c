d2247d0563b55e6d88413367e599912668d4a2b5
popactsa <popactsa.main@yandex.ru>
2026-01-17 16:35:26 +0300

    Complete rework of everything

--- src/error_handling.c
+++ src/error_handling.c
@@ -1,223 +1,46 @@
 #include "error_handling.h"
-#include <errno.h>
-#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include "iomanip.h"
 
-ErrorLevel gChecks  = INFO;
-ErrorLevel gAsserts  = INFO;
+ErrorLevel gMinCheckLevel = INFO;
 
-static const char* ectos(ErrorCode ec) {
+static inline const char* get_ec_str(const ErrorCode ec) {
     switch (ec) {
     case OK:
         return "OK";
-    case ERRST:
-        return "ERRST(Erroneous state)";
-    case MEMALF:
-        return "MEMALF(Memory allocation fail)";
-    case MEMBND:
-        return "MEMBND(Allocated memory boundaries violation)";
-    case MLOGIC:
-        return "MLOGIC(Math logic error)";
-    case CONTRV:
-        return "CONTRV(Contract violation)";
-    case SYSCF:
-        return "SYSCF(System call fail)";
-    default:
+    case UNKNOWN:
         return "UNKNOWN";
-    }
-}
-
-static const char* eltocs(ErrorLevel ec) {
-    switch (ec) {
-    case INFO:
-        return ANSI_BLUE "[INFO]";
-    case WARN:
-        return ANSI_YELLOW "[WARN]";
-    case ERROR:
-    case EXIT:
-        return ANSI_RED "[ERROR]";
+    case MALLOC:
+        return "MALLOC";
+    case PRE:
+        return "PRE";
     default:
-        return ANSI_GREEN "[UNKNOWN]";
+        return "<Not provided>";
     }
 }
 
-void print_log_bool(
-    ErrorLevel  elvl,
-    const char*       fmt,
-    int         line,
-    const char* file,
-    ...) {
-    const char* el_desc;
-    va_list     fmt_args;
+static const char* const error_lvl[] = {"INFO", "WARN", "ERROR", "KILL"};
 
-    va_start(fmt_args);
-    el_desc = eltocs(elvl);
+static const char* const error_lvl_color[] = {
+    ANSI_BLUE, ANSI_YELLOW, ANSI_RED, ANSI_RED};
+
+void log_error(
+    const Error error,
+    int         line,
+    const char* file) {
+    if (!error.ec) {
+        return;
+    }
+    if (error.lvl < gMinCheckLevel) {
+        return;
+    }
     printf(
-        "%1$s Assertion fail at l:%2$d [%3$s]:\n"
-        "%1$s ",
-        el_desc,
-        line,
-        file);
-    vprintf(fmt, fmt_args);
-    printf(ANSI_RESET "\n");
-}
-
-bool leveled_assert(
-    bool        boolean,
-    ErrorLevel  elvl,
-    const char* fmt,
-    int         line,
-    const char* file,
-    ...) {
-    if (gAsserts > elvl) {
-        return false;
-    }
-    if (!boolean) {
-        if (gChecks <= elvl) {
-            va_list args;
-            va_start(args);
-            print_log_bool(elvl, fmt, line, file, args);
-        }
-        if (elvl == EXIT) {
-            exit(EXIT_FAILURE);
-        }
-    }
-    return boolean;
-}
-
-ErrorCode check(
-    ErrorCode   ec,
-    ErrorLevel  elvl,
-    const char* msg,
-    int         line,
-    const char* file) {
-    if (gAsserts > elvl) {
-        return OK;
-    }
-    if (gChecks > elvl) {
-        return ec;
-    }
-    if (ec == OK) {
-        return ec;
-    }
-    print_log(ec, elvl, msg, line, file);
-    if (elvl == EXIT) {
+        "%1$s[%2$s] Error: %3$s at l: %5$d(f: %6$s)\n"
+        "%1$s[%2$s] Description: %4$s\n" ANSI_RESET,
+        error_lvl_color[error.lvl], error_lvl[error.lvl], get_ec_str(error.ec),
+        error.desc ? error.desc : "<Not provided>", line, file);
+    if (error.lvl == EXIT) {
         exit(EXIT_FAILURE);
-    }
-    return ec;
-}
-
-int check_errno(
-    ErrorLevel  elvl,
-    const char* msg,
-    int         line,
-    const char* file) {
-    int saved_errno;
-    saved_errno = errno;
-    errno       = 0;
-    if (gChecks > elvl) {
-        return saved_errno;
-    }
-    if (saved_errno == 0) {
-        return saved_errno;
-    }
-    print_log_errno_snapshot(saved_errno, elvl, msg, line, file);
-    if (elvl == EXIT) {
-        exit(EXIT_FAILURE);
-    }
-    return saved_errno;
-}
-
-int check_errno_omit(
-    ErrorLevel  elvl,
-    const char* msg,
-    int         line,
-    const char* file,
-    ...) {
-    va_list omit;
-    int     saved_errno, omit_cnt;
-    saved_errno = errno;
-    errno       = 0;
-    if (gChecks > elvl) {
-        return saved_errno;
-    }
-    if (saved_errno == 0) {
-        return saved_errno;
-    }
-    va_start(omit);
-    omit_cnt = va_arg(omit, int);
-    for (int i = 0; i < omit_cnt; ++i) {
-        if (saved_errno == va_arg(omit, int)) {
-            return saved_errno;
-        }
-    }
-    print_log_errno_snapshot(saved_errno, elvl, msg, line, file);
-    if (elvl == EXIT) {
-        exit(EXIT_FAILURE);
-    }
-    return saved_errno;
-}
-
-void print_log(
-    ErrorCode   ec,
-    ErrorLevel  elvl,
-    const char* msg,
-    int         line,
-    const char* file) {
-    const char *ec_desc, *el_desc;
-
-    ec_desc = ectos(ec);
-    el_desc = eltocs(elvl);
-    if (msg) {
-        fprintf(
-            stderr,
-            "%1$s Error %4$s occured at l:%2$d [%3$s]\n"
-            "%1$s %5$s\n" ANSI_RESET,
-            el_desc,
-            line,
-            file,
-            ec_desc,
-            msg);
-    } else {
-        fprintf(
-            stderr,
-            "%1$s Error %4$s occured at l:%2$d [%3$s]\n" ANSI_RESET,
-            el_desc,
-            line,
-            file,
-            ec_desc);
-    }
-}
-
-void print_log_errno_snapshot(
-    int         errno_snapshot,
-    ErrorLevel  elvl,
-    const char* msg,
-    int         line,
-    const char* file) {
-    const char* el_desc;
-
-    el_desc = eltocs(elvl);
-    if (msg) {
-        fprintf(
-            stderr,
-            "%1$s Error %4$s occured at l:%2$d [%3$s]\n"
-            "%1$s %5$s\n" ANSI_RESET,
-            el_desc,
-            line,
-            file,
-            strerror(errno_snapshot),
-            msg);
-    } else {
-        fprintf(
-            stderr,
-            "%1$s Error %4$s occured at l:%2$d [%3$s]\n" ANSI_RESET,
-            el_desc,
-            line,
-            file,
-            strerror(errno_snapshot));
     }
 }
